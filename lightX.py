66678a52862fd75ab3ad0207531d09bb6772d6b2
DevPrimeYT <hadi.jaffrey@hotmail.com>
2021-10-11 19:46:56 +1100

    New pip feature

--- lightX.py
+++ lightX.py
@@ -1,29 +1,38 @@
 import os
 import requests
 from zipfile import ZipFile
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+
 crash = 0
 example_package = 'https://github.com/Lightspace-Official/lightX-DevBox/blob/packages/example.lxpkg?raw=true'
 example_package_r = requests.get(example_package, allow_redirects=True)
 compiler_package = 'https://github.com/Lightspace-Official/lightX-DevBox/blob/packages/compiler.lxpkg?raw=true'
 compiler_package_r = requests.get(compiler_package, allow_redirects=True)
+print('lighX DevBox Shell By Lightspace Org, Fully Open Source ')
 while (crash == 0):   
     text = input('lx >')
 
     if text == ('example'):
      print('Welcome to lightX DevBox')
+     
     elif text == ('mk program'):
         print('Using IDE to make a new program...')
         os.system("ide.py");
+
     elif text == ('pkg program'):
         print('Directing you to packager...')
         os.system("Package.py");
+
     elif text == ('run program'):
         print('Which program? (Input relative path)')
         program_run = input('')
         os.system('python ' + program_run);
+
     elif text == ('unpkg program'):
         print('Directing you to unpackager...')
         os.system('Unpackage.py');
+
     elif text == ('install pkg'):
         progam_to_be_installed = input('Which program?')
 
@@ -51,10 +60,21 @@ while (crash == 0):
         else:
             print('Package not found.')
 
-    elif text == ('cd'):
+    elif text == ('cd'): #cd command
         cd = input('Which folder? ')
         os.chdir(cd)
         print('Changed to ' + cd)
+
+    elif text == ('lx pip'): #This command uses pyPI to install external pip packages
+        pipPackage = input('Which pip module would you like to install? ')
+        os.system('pip install ' + pipPackage)
+        print('All Done!')
+
+    elif text == ('lx requirements.txt'): #This command downloads and installs all the requirements that a lX program needs
+        os.system('pip install -r requirements.txt')
+        print('Pip packages installed!')
+    elif text == ('exit'): #Exit command
+        exit()    
     else:
         print('Error - Invalid command')
     
