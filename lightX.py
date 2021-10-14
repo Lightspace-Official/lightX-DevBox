168ed468fddff86b76b07846be8a4d5d4a959a50
DevPrimeYT <hadi.jaffrey@hotmail.com>
2021-10-15 09:42:37 +1100

    Update lightX.py

--- lightX.py
+++ lightX.py
@@ -5,38 +5,38 @@ from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 
 crash = 0
-example_package = 'https://github.com/Lightspace-Official/lightX-DevBox/blob/packages/example.lxpkg?raw=true'
+example_package = 'https://github.com/Lightspace-Official/lightX-DevBox/blob/packages/example.lxpkg?raw=true' # Example package link
 example_package_r = requests.get(example_package, allow_redirects=True)
-compiler_package = 'https://github.com/Lightspace-Official/lightX-DevBox/blob/packages/compiler.lxpkg?raw=true'
+compiler_package = 'https://github.com/Lightspace-Official/lightX-DevBox/blob/packages/compiler.lxpkg?raw=true' # Compiler Package link
 compiler_package_r = requests.get(compiler_package, allow_redirects=True)
-print('lightX DevBox Shell By Lightspace Org, Fully Open Source ')
+print('lightX DevBox Shell By Lightspace Org, Fully Open Source ') # Intro 
 while (crash == 0):   
     text = input('lx >')
 
-    if text == ('example'):
+    if text == ('example'): # Example command
      print('Welcome to lightX DevBox')
      
-    elif text == ('mk program'):
+    elif text == ('mk program'): # Opens the IDE to make a new program
         print('Using IDE to make a new program...')
         os.system("ide.py");
 
-    elif text == ('pkg program'):
+    elif text == ('pkg program'): # Packages a program folder
         print('Directing you to packager...')
         os.system("Package.py");
 
-    elif text == ('run program'):
+    elif text == ('run program'): # Runs a .lxp program
         print('Which program? (Input relative path)')
         program_run = input('')
         os.system('python ' + program_run);
 
-    elif text == ('unpkg program'):
+    elif text == ('unpkg program'): #Unpackages a lxpkg file
         print('Directing you to unpackager...')
         os.system('Unpackage.py');
 
-    elif text == ('install pkg'):
+    elif text == ('install pkg'): # This command is used to install a package from lXPI
         progam_to_be_installed = input('Which program?')
 
-        if progam_to_be_installed == ('example'):
+        if progam_to_be_installed == ('example'): #example pkg install command
             open('example.lxpkg', 'wb').write(example_package_r.content)
             file_name = ('example.lxpkg')
             with ZipFile(file_name, 'r') as zip:
@@ -47,7 +47,7 @@ while (crash == 0):
              print('example Installed')
              print('Use cd example + run program to start example.')
 
-        elif progam_to_be_installed == ('compiler'):
+        elif progam_to_be_installed == ('compiler'): # compiler 'if' command
             open('compiler.lxpkg', 'wb').write(compiler_package_r.content)
             file_name2 = ('compiler.lxpkg')
             with ZipFile(file_name2, 'r') as zip:
@@ -73,10 +73,19 @@ while (crash == 0):
     elif text == ('lx requirements.txt'): #This command downloads and installs all the requirements that a lX program needs
         os.system('pip install -r requirements.txt')
         print('Pip packages installed!')
+
     elif text == ('exit'): #Exit command
-        exit()    
+        exit()
+    elif text == ('help'): # Help command
+        help = input('Which command do you need help with?')
+        if help == ('mk program'):
+            print('This program opens the lX IDE for you to make a new program.')
+        elif help == ('pkg program'):
+            print('This command opens the Unpackager which allows you to unpkg a .lxpkg file.')
+        else:
+            print('Help for this command not found')   
     else:
-        print('Error - Invalid command')
+        print('Error - Invalid command') # Error message
     
     
      
